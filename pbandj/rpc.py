9d42f7283dca9614fffd0ed41984525881930e4f
walker.zach@gmail.com <walker.zach@gmail.com@2250a6b6-9e6b-11de-81d6-937ba7f528c2>
2012-07-20 23:04:26 +0000

    Fixing rpc call signature

--- pbandj/rpc.py
+++ pbandj/rpc.py
@@ -21,8 +21,8 @@ import threading
 from time import time, sleep
 
 # Third party imports
-from protobuf.channel import SocketRpcChannel
-from protobuf.server import SocketRpcServer
+from protobuf.socketrpc.channel import SocketRpcChannel
+from protobuf.socketrpc.server import SocketRpcServer
 
 # Module imports
 from modelish.pb.service import Service
@@ -33,131 +33,43 @@ RPC_PORT = 8091
 
 class ServiceThread(threading.Thread):
 
-    def __init__(self, mapped_module, port, daemon=False):
+
+    def __init__(self, mapped_module, service_module, port, daemon=False):
         threading.Thread.__init__(self)
-        self.module = mapped_module.load_pb2()
         self.mapped_module = mapped_module
-#        for service in self.mapped_module.services:
-#            print type(service)
-#            if isinstance(service, Service):
-#                print "is a service"
-#                self.services.append(service)
+        self.service_module = service_module
+        self.service_pb2_module = service_module.load_pb2()
         self.port = port
         self.setDaemon(daemon)
 
-#            def _createHandler(self, func):
-#                def handler(service_impl, controller, request, done):
-#                    try:
-#                        result = func(request)
-#                    except Exception, e:
-#                        controller.handleError(e, e.__str__())
-#                    done.run(result)
+
+    def protobuf_rpc_closure(self, handler):
+        ''' Returns a function implementing the expected method signature for
+        a protobuf rpc handler by wrapping the implemented handler function
+        '''
+        def protobuf_rpc_wrapper(controller, request, callback):
+            try:
+                result = handler(request,
+                                 mapped_module=self.mapped_module,
+                                 service_module=self.service_module)
+                callback.run(result)
+            except Exception, e:
+                controller.handleError(1, e.message)
+        return protobuf_rpc_wrapper
+
 
     def run(self):
         """ Start service thread """
         # Create a server instance
+        import ipdb; ipdb.set_trace();
         server = SocketRpcServer(self.port,host='')
-        for service in self.mapped_module.services:
-            service_class = self.module.__dict__[service.name]
-#            handlers = {}
-#
-#            # Link each method to its handler
-#            for method in service.rpc.values():
-#                print "handling method " + method.name
-#                #handlers[method.name] = self._createHandler(method.handler)
-#                print "using handler " + method.handler
-#                handlers[method.name] = method.handler
-
-            # Extend Service class and add handlers
-            newservice = type(service.name + "Impl",
-                          (service_class,), self.mapped_module.service_handlers[service.name])
-            server.registerService(newservice())
+        for service in self.service_module.services:
+            service_class = getattr(self.service_pb2_module, service.name)
+            newservice = service_class()
+            for method_name, handler in self.service_module.service_handlers[service.name].items():
+                # Attatch a handler to the service class
+                setattr(newservice, method_name, self.protobuf_rpc_closure(handler))
+            server.registerService(newservice)
 
         # Start the server
         server.run();
-
-
-
-class ProtoBufRpcRequest(object):
-    """ Class abstracting the Protocol Buffer RPC calls for a supplied
-        service stub.
-    """
-  
-    def __init__(self, service_stub_class, port=RPC_PORT, host=RPC_HOST):
-        """ Contruct a new ProtoBufRpcRequest and return it.
-
-            Accepted Arguments:
-            service_stub_class -- (Service_Stub) The client side RPC
-                                  stub class produced by protoc from
-                                  the .proto file
-            port -- (Integer) The port on which the service is running
-                    on the RPC server. Default is value of
-                    pond_client.RPC_PORT
-            host -- (String) The hostname or IP address of the server
-                    running the RPC service. Default is value of
-                    pond_client.RPC_HOST
-        """
-        self.service_stub_class = service_stub_class
-        self.port               = port
-        self.host               = host
-        
-        #go through service_stub methods and add a wrapper function to 
-        #this object that will call the method with only the msg as the arg
-        for method in service_stub_class.GetDescriptor().methods:
-            #Add service methods to the this object
-            self.__dict__[method.name] = \
-                lambda msg, timeout, service=self, method=method.name : service.call(
-                    service_stub_class.__dict__[method], msg, timeout)
-            
-            #This is just a convenience that adds service methods to the
-            #instance objects namespace.  No longer do it this way since
-            #the  closures above are cleaner
-            #self.__dict__[method.name] = \
-            #    service_stub_class.__dict__[method.name]
-
-    
-    def call(self, rpc, msg, timeout):
-        """ Save the object that has been created and return the response.
-            Will timeout after timeout ms if response has not been received
-            
-            Accepted Arguments:
-            timeout -- (Integer) ms to wait for a response before returning
-        """
-                
-        # Define local callback function to handle RPC response
-        # and initialize result dict
-        result = {"done":False, "success":False, "response":None,
-                        "error_msg":"timeout", "error_code":None}
-        #Can't use just a simple boolean for 'done' because of the way it is
-        #referenced in the callback.  The scope isn't clear and python
-        #will redefine 'done' rather than change the value unless explicitly
-        #defined as 'global done'
-        #done = False
-        def callback(request, response):
-            result["response"]  = response
-            result["done"]      = True
-            result["error_msg"] = ""
-            result["success"]   = True
-        
-        #Setup the RPC channel
-        channel     = SocketRpcChannel(host=self.host, port=self.port)
-        controller  = channel.newController()
-        service     = self.service_stub_class(channel)
-        
-        #Make the RPC call and pass in a generic object with a
-        #function called "run" linked to callback
-        try:
-            #Attempting to do something similar to a JAVA anonymous class
-            #Create an instalce of a new generic type initialized with a
-            #dict that has a run method pointing to the callback function
-            rpc(service, controller, msg, type("", (), {"run":callback})())
-        except Exception, info:
-            print controller.error
-        except:
-            print controller.error
-        end = time() + (timeout / 1000)
-        
-        #Tight loop waiting for timeout or callback to set done to True 
-        while time() < end and not result["done"]:
-            pass
-        return result
