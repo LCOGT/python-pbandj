1c2b16bd90a63e7feb0ecc2926fcf1ee70a49c4e
walker.zach@gmail.com <walker.zach@gmail.com@2250a6b6-9e6b-11de-81d6-937ba7f528c2>
2012-07-20 23:05:50 +0000

    Cleaning

--- pbandj/management/commands/run_protobuf_service.py
+++ pbandj/management/commands/run_protobuf_service.py
@@ -13,8 +13,7 @@ from django.conf import settings
 from django.db import models
 
 from pbandj.rpc import ServiceThread
-#from pbandj import pbandj
-#from pbandj.model import ProtocolBuffer
+from pbandj import util
 
 
 PBANDJ_SERVICE_MODULES = getattr(settings, 'PBANDJ_SERVICE_MODULES', {})
@@ -43,52 +42,17 @@ class Command(BaseCommand):
         
         # See if the app exists
         app = app.split(".")[-1]
-        service_mod = app + '.' + PBANDJ_SERVICE_MODULES.get(app, app + "." + PBANDJ_SERVICE_MODULE)
-        try:
-            service_mod = __import__(service_mod, fromlist=[app])
-        except Exception, e:
-            print e
-            return
         
         # Load the pickled service module
-        pickeled_srvc_file = open(app + "/" + "pickeled_pbandj.service", 'r')
-        mapped_module = pickle.load(pickeled_srvc_file)
-        
-#        # Generate service binding info 
-#        pb = ProtocolBuffer(app + '_' + 'services')
-#        for service_name, methods in pbandj.service_registry.items():
-#            for service_method in methods:
-#                pb.addRpc(service_name, service_method['method'], service_method['input'].generate_protocol_buffer(), service_method['output'].generate_protocol_buffer(), service_method['handler'])
-        
-#        # Import service rpc implementation
-#        pb2_path = options.get('pb2')
-#        pb2_mod = None
-#        if pb2_path:
-#            mod_name,file_ext = os.path.splitext(os.path.split(pb2_path)[-1])
-#            if file_ext == '.py':
-#                pb2_mod = imp.load_source(mod_name, pb2_path)
-#            elif file_ext == '.pyc':
-#                pb2_mod = imp.load_compiled(mod_name, pb2_path)
-#            else:
-#                print "Don't understand pb2 value %s" % pb2_path
-#                sys.exit(1)
+        mapped_module = util.restore_module(app)
+        service_module = util.restore_service_module(app)
         
         
         # Bind handlers to rpc implementation
-        sthread = ServiceThread(mapped_module, int(port), daemon)
+        sthread = ServiceThread(mapped_module, service_module, int(port), daemon)
         sthread.start()
         while(sthread.is_alive()):
             sthread.join(1)     
-#        mod_name = pbandj.genMod(pb)
         
         
 
-
-#if __name__ == "__main__":
-#    from obsdb import pb
-#    pbandj.genMod(pb.model, "protobuf/src/main/lcogt/pond")
-#    pbandj.genMod(pb.services(), "protobuf/src/main/lcogt/pond")
-#    pbandj.genMod(pb.services())
-#    
-#    pbandj.genMod(pb.model, "client/python/src/lcogt/pond")
-#    pbandj.genMod(pb.services(), "client/python/src/lcogt/pond")   
